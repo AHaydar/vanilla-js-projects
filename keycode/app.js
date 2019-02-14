01ad01bebc1d61b770d3d22de18cc302d548c783
Ali Haydar <ali.haydar85@gmail.com>
2019-02-15 01:02:41 +1300

    Show the key name on the page as well on the same line with the corresponding keycode

--- keycode/app.js
+++ keycode/app.js
@@ -1,13 +1,22 @@
 let container = document.querySelector('#container');
 document.addEventListener('keydown', function(event) {
-    let keyCode = document.querySelector('.key-code');
-    console.log(keyCode);
-    if(keyCode !== null){
-        keyCode.remove();
+    event.preventDefault();
+    let keyContainer = document.querySelector('.key-container');
+    if(keyContainer !== null){
+        keyContainer.remove();
     }
+    keyContainer = document.createElement('div');
+    keyContainer.setAttribute('class', 'key-container');
     container.setAttribute('class', 'align-content-vertical');
+    container.appendChild(keyContainer);
+
+    let theKeyName = document.createElement('label');
+    theKeyName.setAttribute('class', 'key-name');
+    theKeyName.innerText = event.key;
+    keyContainer.appendChild(theKeyName);
+
     let theKeyCode = document.createElement('label');
     theKeyCode.setAttribute('class', 'key-code')
     theKeyCode.innerText=event.keyCode;
-    container.appendChild(theKeyCode);
+    keyContainer.appendChild(theKeyCode);
 });
