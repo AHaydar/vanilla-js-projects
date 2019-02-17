59425c53d10ce4223d5f2845a7bc1e0364f49a17
Ali Haydar <ali.haydar85@gmail.com>
2019-02-18 06:14:20 +1300

    Change CSS on mouse over, and mouse out

--- drum-kit/app.js
+++ drum-kit/app.js
@@ -6,8 +6,18 @@ let buttonsArray = [...buttons];
 
 // for each button in the array add event lister that retrieves the corresponding audio tag and plays it
 buttonsArray.forEach(function playAudio(htmlButton) {
+    htmlButton.addEventListener('mouseover', function(e){
+        htmlButton.style.backgroundColor = 'white';
+    });
+
+    htmlButton.addEventListener('mouseout', function(e){
+        htmlButton.style.backgroundColor = 'bisque';
+    });
+
     htmlButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        htmlButton.style.borderColor = 'green';
         let audio = htmlButton.querySelector('audio');
         audio.play();
-    })
+    });
 })
